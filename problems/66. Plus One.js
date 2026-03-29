14eaac26c1e5b48daa4d014b04bda3a04c73a07d
takuyadev <takuya.k.toyokawa@gmail.com>
2026-03-28 20:31:04 -0700

    feat: complete plus one

---
+++ problems/66. Plus One.js
@@ -0,0 +1,25 @@
+// I didn't find this hard
+// Reverse the array, two pointer solution.
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] >= 9) {
+      digits[i] = 0;
+
+      if (i === 0) {
+        digits.unshift(1);
+        return digits;
+      }
+    } else {
+      digits[i] += 1;
+      return digits;
+    }
+  }
+
+  return digits;
+};
