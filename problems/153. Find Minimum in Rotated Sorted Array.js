c29964947fa90ba19fce2950ea093c48fd724651
takuyadev <takuya.k.toyokawa@gmail.com>
2026-04-14 21:26:46 -0700

    feat: add solution to find minimum in rotated sorted array

--- problems/153. Find Minimum in Rotated Sorted Array.js
+++ problems/153. Find Minimum in Rotated Sorted Array.js
@@ -34,3 +34,27 @@ var findMin = function (nums) {
 
   return nums[low];
 };
+
+// Shorter version. You don't need to figure out center; the smaller number will always be on the right side of the array.
+// Time complexity: O(log n)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+    let high = nums.length - 1;
+    let low = 0;
+
+    while (low < high) {
+        const mid = Math.floor((high + low) / 2);
+
+        if (nums[mid] < nums[high]) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return nums[low];
+};
