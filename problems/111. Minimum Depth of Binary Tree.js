8c63e36119baeb2c8df0e9d92db24028f780cad1
takuyadev <takuya.k.toyokawa@gmail.com>
2026-08-20 16:09:08 +0900

    feat: add dfs solution to minimum depth of binary tree

--- problems/111. Minimum Depth of Binary Tree.js
+++ problems/111. Minimum Depth of Binary Tree.js
@@ -34,3 +34,49 @@ var traverse = function(root) {
     return 1 + Math.min(rightDepth, leftDepth)
   }
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var minDepth = function(root) {
+  if (!root) {
+    return 0;
+  }
+
+  const queue = [root]
+  let depth = 1;
+
+  while (queue.length) {
+    const size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+
+      if (node.left === null && node.right === null) {
+        return depth;
+      }
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    depth++;
+  }
+
+  return depth
+};
