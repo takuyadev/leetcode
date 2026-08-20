ccd6b7439370586d0b705fea113881d84baf696f
takuyadev <takuya.k.toyokawa@gmail.com>
2026-08-20 16:01:36 +0900

    feat: complete minimum depth of binary tree

---
+++ problems/111. Minimum Depth of Binary Tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var minDepth = function(root) {
+  return traverse(root, 0);
+};
+
+var traverse = function(root) {
+  if (root === null) {
+    return 0;
+  }
+
+  const leftDepth = traverse(root.left);
+  const rightDepth = traverse(root.right);
+
+  // If left is non-existant, just add right depth
+  if (root.left === null) {
+    return rightDepth + 1;
+    // Vice versa
+  } else if (root.right === null) {
+    return leftDepth + 1;
+  } else {
+    // If in recursion left is not a value, do not Math.min() because 0 will always overwrite
+    return 1 + Math.min(rightDepth, leftDepth)
+  }
+}
